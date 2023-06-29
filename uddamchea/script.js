7d90d1f06421be605cabd00ed28e793fcfa38c44
uddamchea <uddamrchea@gmail.com>
2023-06-28 20:07:17 -0700

    Landing Page

---
+++ uddamchea/script.js
@@ -0,0 +1,8 @@
+// background scroll
+window.addEventListener('scroll', function() {
+    const background = document.getElementById('background');
+    const yOffset = window.pageYOffset;
+    background.style.backgroundPositionY = -yOffset * 0.5 + 'px';
+  });
+
+//
